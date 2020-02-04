7883ddd44d1b6993890dc5955f1e4c62247964e9
Rafael Andrade de Oliveira <eu.rafa@gmail.com>
2020-02-04 16:45:27 -0300

    Spring Initializr project generated

---
+++ src/test/java/br/com/softdesign/career/votingservice/VotingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softdesign.career.votingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
