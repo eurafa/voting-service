c019afb4e1f1116a6fc1a8d89bcce71d503fb0aa
Rafael Andrade de Oliveira <eu.rafa@gmail.com>
2020-02-04 23:45:30 -0300

    MR cleanup

--- src/main/java/br/com/softdesign/career/votingservice/VotingServiceApplication.java
+++ src/main/java/br/com/softdesign/career/votingservice/VotingServiceApplication.java
@@ -3,13 +3,11 @@ package br.com.softdesign.career.votingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.Generated;
-
 @SpringBootApplication
 public class VotingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VotingServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VotingServiceApplication.class, args);
+    }
 
 }
